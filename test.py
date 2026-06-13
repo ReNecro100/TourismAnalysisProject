95e4837f4c503d93736d775b847721876ea6f7a1
ReNecro100 <jarabotaju2007@gmail.com>
2026-06-13 11:24:22 +0300

    I forgot to commit that API

---
+++ test.py
@@ -0,0 +1,12 @@
+# import geopandas as gpd
+# import geodatasets
+# import matplotlib.pyplot as plt
+#
+# # Load the built-in world dataset
+# world = gpd.read_file(geodatasets.get_path("naturalearth.land"))
+#
+# world = world[world["name"] == "Russia"]
+# # Plot the map
+# world.plot()
+# plt.title("Static World Map")
+# plt.show()
